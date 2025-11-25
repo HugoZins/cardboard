1202796efda55fcb1955ff59daf11d9d285a8356
Hugo Zins <122614297+HugoZins@users.noreply.github.com>
2025-11-25 21:27:48 +0100

    fix: 404.html (toujours plus)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,15 +9,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        notFound: './index.html'
-      },
-      output: {
-        entryFileNames: 'assets/[name].js',
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'index.html') return '404.html'
-          return 'assets/[name].[ext]'
-        }
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'index.html')
       }
     }
   }
